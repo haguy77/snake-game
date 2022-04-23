e2499996b45429f16fef1a37d9fd727f33f854bd
haguy77 <idelchikhaguy@gmail.com>
2022-04-23 19:58:13 +0300

    converted basic surface and snake to OOP

---
+++ snake.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
+
+    def move_left(self):
+        self.x = self.x - 10
+        self.draw()
+
+    def move_right(self):
+        self.x = self.x + 10
+        self.draw()
+
+    def move_up(self):
+        self.y = self.y - 10
+        self.draw()
+
+    def move_down(self):
+        self.y = self.y + 10
+        self.draw()
